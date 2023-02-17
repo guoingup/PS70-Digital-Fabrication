d35d96e156b684761742cd2cc8359147ab207e02
guoingup <107078860+guoingup@users.noreply.github.com>
2023-02-17 18:13:45 -0500

    Kinetic Sculpture Videos/Pictures

---
+++ 04_microcontroller/sketch_feb17a/sketch_feb17a.ino
@@ -0,0 +1,67 @@
+//#include <Servo.h> 
+//
+//
+class Spin
+{
+  // Class Member Variables
+  // These are initialized at startup
+  const int A1A;      
+  const int A1B;
+  long OnSpinInterval;
+  long OffSpinInterval; 
+
+  // These maintain the current state
+  int motorState;                // ledState used to set the LED
+  unsigned long previousMillis;   // will store last time LED was updated
+
+  //Constructor - creates a Spinning Motor
+  public:
+  Spin(int pinA, int pinB, long ONinterval, long OFFinterval)
+  {
+    A1A = pinA;
+    A1B = pinB;
+
+    pinMode(A1A, OUTPUT);
+    pinMode(A1B, OUTPUT);
+
+    digitalWrite(A1A, LOW);
+    digitalWrite(A1B, LOW);
+
+    motorState = LOW;
+
+    ONSpinInterval = ONinterval;
+    OFFSpinInterval = OFFinterval;
+    previousMillis = 0;
+  }
+  
+  void Update()
+  {
+    // check to see if it's time to change the state of the LED
+    unsigned long currentMillis = millis();
+     
+    if((motorState == LOW) && (currentMillis - previousMillis >= OffSpinInterval))
+    {
+      digitalWrite(A1A, HIGH);
+      motorState = HIGH;
+      previousMillis = currentMillis;
+    }
+    else if ((motorState == HIGH) && (currentMillis - previousMillis >= OnSpinInterval))
+    {
+      digitalWrite(A1A, LOW);
+      motorState = LOW;
+      previousMillis = currentMillis; // Remember the time
+    }
+  }
+};
+
+int pinA, int pinB, long ONinterval, long OFFinterval;
+
+Spin motor1(3,4,1000,500);
+
+void setup() {
+  
+}
+
+void loop() {
+  motor1.Update();
+}
